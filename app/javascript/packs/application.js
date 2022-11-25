75a064e9a7859a3ef9bbb6783813046f1b1d0850
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-11-25 14:02:25 +0000

    [応用課題7d]星５段階評価実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,24 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+import jQuery from "jquery"
+import raty from 'raty-js'
 import "popper.js"
 import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+global.$ = jQuery;
+window.$ = jQuery;
+
+ import Raty from "raty.js"
+ window.raty = function(elem,opt){
+    let raty =  new Raty(elem,opt)
+    raty.init();
+    return raty;
+}
